104779454e0b06cfaa9615c67af89dcd6eb74125
Amy M Haddad <amy.m.haddad@gmail.com>
2020-01-13 07:59:55 -0500

    handling merge conflict

--- Lesson08-JS-V/homework/homework.js
+++ Lesson08-JS-V/homework/homework.js
@@ -1,6 +1,5 @@
 // Do not change any of the function names
 
-
 // create an ES6 class or a constructor function called User
 // it should accept an options object with username, name, email, and password properties
 // in the constructor set the username, name, email, and password properties
@@ -8,51 +7,43 @@
 // {{name}} should be the name set on each instance
 // return the class
 
+
 function User (options) {
-  this.username = options.username;
-  this.name = options.name;
-  this.email = options.email;
-  this.password = options.password;
-}
-
-function createUserClass() {
+    this.username = options.username;
+    this.name = options.name;
+    this.email = options.email;
+    this.password = options.password;
+  }
+  
+ function createUserClass() {
     return User;
-}
-
-User.prototype.sayHi = function() {
+ }
+ 
+ User.prototype.sayHi = function() {
     return `Hello, my name is ${this.name}`;
-};
-
-
-// const userConstructor = createUserClass();
-// const user = new userConstructor();
-// user.sayHi();
-function addPrototypeMethod(Constructor) {
+ };
+ 
+ 
+ // add a method to the constructor's prototype
+ // the method should be called 'sayHi' and should return the string 'Hello World!'
+ function addPrototypeMethod(Constructor) {
     Constructor.prototype.sayHi = function(){
         return 'Hello World!';
     };
-}
-
-
-
-// add a method to the string constructor's prototype that returns a reversed copy of the string
-// name this method reverse
-// hint:
-// you will need to use 'this' inside of reverse
-
-function addReverseString(string) {
-    // this.string = string;
- 
-    // addReverseString.prototype.reverse = function() {
-    //    let listWord = [];
-    //    for (let i=this.string.length-   1; i >=0; i--){
-    //       listWord.push(this.string[i]);
-    //    }
-    //    return listWord.join('');
-    // };  
  }
-
-
+ 
+ // add a method to the string constructor's prototype that returns a reversed copy of the string
+ // name this method reverse
+ // hint:
+ // you will need to use 'this' inside of reverse
+ // function addReverseString() {
+ // }
+ 
+ function addReverseString() {
+    String.prototype.reverse = function () {
+        return this.split('').reverse().join('');
+        };
+ }
 
 // Do not modify code below this line.
 // --------------------------------
--- Lesson08-JS-V/homework/tests/JSV.test.js
+++ Lesson08-JS-V/homework/tests/JSV.test.js
@@ -5,6 +5,7 @@ const {
   addReverseString,
 } = require('../homework');
 
+<<<<<<< HEAD
 describe('createUserClass()', function() {
 	it('should return a user constructor that correctly builds user objects', function() {
 		const User = createUserClass();
@@ -27,6 +28,30 @@ describe('addPrototypeMethod(Constructor)', function() {
 	});
 });
 
+=======
+// describe('createUserClass()', function() {
+// 	it('should return a user constructor that correctly builds user objects', function() {
+// 		const User = createUserClass();
+// 		const user = new User({username: 'SunJieMing', name: 'Ben', email: 'ben@lambdaschool.com', password: 'LS Rocks!' });
+// 		expect(user.username).toBe('SunJieMing');
+// 		expect(user.name).toBe('Ben');
+// 		expect(user.email).toBe('ben@lambdaschool.com');
+// 		expect(user.password).toBe('LS Rocks!');
+// 	});
+// });
+
+describe('addPrototypeMethod(Constructor)', function() {
+	it('should add the method sayHi to the constructor', function() {
+		function Test() {
+			this.test = true;
+		}
+		addPrototypeMethod(Test);
+		const test = new Test();
+		expect(test.sayHi()).toBe('Hello World!');
+	});
+});
+
+>>>>>>> 97a6b2b... WIP - prototypes
 describe('addReverseString(StringPrototype)', function(){
 	it('should add a reverse string method to the String prototype that returns a reversed version of the string', function() {
 		addReverseString();
